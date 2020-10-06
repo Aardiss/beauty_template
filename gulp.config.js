64588d37637a4959491f959c8aa96d507447f02a
Anastasia <aardiss@mail.ru>
2020-10-06 15:28:47 +0300

    gulp-сборка

---
+++ gulp.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  DIST_PATH: "dist",
+  SRC_PATH: "src",
+  STYLE_LIBS: ["node_modules/normalize.css/normalize.css"
+],
+  JS_LIBS: [
+  "node_modules/jquery/dist/jquery.min.js",
+  "node_modules/bxslider/dist/jquery.bxslider.js"
+  ]
+};
